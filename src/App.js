063936f1d5ecb00f258e5709fe40090a478ed5a3
Mironov Ernest <erik1297e@gmail.com>
2022-05-30 17:40:06 +0300

    init project with tailwind and router

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import { Outlet, Link } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-sky-500 h-screen">
+      <h1 className='text-8xl font-bold text-white text-center'>HA-HA-thon</h1>
+      {/* <nav
+        style={{
+          borderBottom: "solid 1px",
+          paddingBottom: "1rem",
+        }}
+      >
+        <Link to="/invoices">Invoices</Link> |{" "}
+        <Link to="/expenses">Expenses</Link>
+      </nav> */}
+      <Outlet />
     </div>
   );
 }
