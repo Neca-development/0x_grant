012ebfb0c8fe9d365af6e615b2e02b5653adcf25
AndreyBS1 <andrey.babenkov.00@gmail.com>
2022-05-31 18:45:23 +0300

    Fixed input styles

--- src/components/NftCard.tsx
+++ src/components/NftCard.tsx
@@ -1,4 +1,4 @@
-import { FunctionComponent, useMemo } from "react";
+import { FunctionComponent } from "react";
 import unistoryLogo from "../assets/icons/unistory_logo.svg";
 import { IToken } from "../models/interfaces";
 
@@ -11,22 +11,10 @@ export interface ITokenCardProps {
 const NftCard: FunctionComponent<ITokenCardProps> = (props) => {
   const { data, onClick, externalClasses } = props;
 
-  const contractAddress = useMemo(
-    () =>
-      `${data.collectionAddress?.substring(
-        0,
-        4
-      )}...${data.collectionAddress?.substring(
-        data.collectionAddress.length - 6,
-        data.collectionAddress.length
-      )}`,
-    [data.collectionAddress]
-  );
-
   return (
     <article
       onClick={onClick}
-      className={`rounded-lg overflow-hidden border border-gray-100 shadow-[4px_4px_10px_rgba(224,224,224,0.25)] cursor-pointer ${externalClasses}`}
+      className={`rounded-lg overflow-hidden border border-gray-100 shadow-[4px_4px_10px_rgba(224,224,224,0.25)] ${externalClasses}`}
     >
       <div className="aspect-square bg-gray-200 relative flex">
         {data.image ? (
@@ -45,15 +33,13 @@ const NftCard: FunctionComponent<ITokenCardProps> = (props) => {
         <div className="text-xl text-center text-dark-gray font-semibold mt-10 mb-4">
           {data.collectionName}
         </div>
-        {/* <div className="text-dark-gray font-semibold mb-3"> */}
-          <input
-            className="w-full py-1 text-center border-0 border-b-2 border-[rgba(20,20,20,1)] focus:border-0 focus:border-b-2 focus:border-black focus:ring-0"
-            type="text"
-            placeholder="Enter the amount of NFT"
-            pattern="^[0-9]+$"
-          />
-        {/* </div> */}
-        <button className="w-fit my-6 py-2 px-6 rounded-lg bg-blue text-white font-semibold">
+        <input
+          className="w-full py-2 text-center font-semibold text-black border-0 border-b-2 border-[rgba(20,20,20,1)] focus:border-0 focus:border-b-2 focus:ring-0 focus:outline-0"
+          type="text"
+          placeholder="Enter the amount of NFT"
+          pattern="^[0-9]+$"
+        />
+        <button className="w-fit my-6 py-3 px-8 rounded-lg bg-blue text-white font-semibold">
           Mint NFT
         </button>
       </div>
