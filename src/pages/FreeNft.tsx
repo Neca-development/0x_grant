1870f0733f286cb12e568bbd03f5d349097b53ac
Mironov Ernest <erik1297e@gmail.com>
2022-05-31 22:17:03 +0300

    fix free nft tokens

--- src/pages/FreeNft.tsx
+++ src/pages/FreeNft.tsx
@@ -2,38 +2,38 @@ import NftCard from "../components/NftCard";
 import { IToken } from "../models/interfaces";
 
 function FreeNft() {
-  const tokens: IToken[] = [
-    {
-      id: 1,
-      symbol: "Test 1",
-      image: "",
-      collectionName: "Test 1",
-      contractAddress: "1",
-    },
-    {
-      id: 2,
-      symbol: "Test 2",
-      image: "",
-      collectionName: "Test 2",
-      contractAddress: "2",
-    },
-  ];
+	const tokens: IToken[] = [
+		{
+			tokenId: 1,
+			symbol: "Test 1",
+			image: "",
+			collectionName: "Test 1",
+			contractAddress: "1",
+		},
+		{
+			tokenId: 2,
+			symbol: "Test 2",
+			image: "",
+			collectionName: "Test 2",
+			contractAddress: "2",
+		},
+	];
 
-  return (
-    <div className="container mx-auto pt-12 bg-white">
-      <h1 className="text-center mb-20 font-semibold text-6xl w-3/4 mx-auto leading-snug">
-        Get free NFT
-      </h1>
-      <div className="flex justify-around">
-        {tokens.map((token) => (
-          <NftCard
-            key={token.id + token.contractAddress}
-            data={token}
-          ></NftCard>
-        ))}
-      </div>
-    </div>
-  );
+	return (
+		<div className="container mx-auto pt-12 bg-white">
+			<h1 className="text-center mb-20 font-semibold text-6xl w-3/4 mx-auto leading-snug">
+				Get free NFT
+			</h1>
+			<div className="flex justify-around">
+				{tokens.map((token) => (
+					<NftCard
+						key={token.tokenId + token.contractAddress}
+						data={token}
+					></NftCard>
+				))}
+			</div>
+		</div>
+	);
 }
 
 export default FreeNft;
