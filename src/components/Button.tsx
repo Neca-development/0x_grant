2bf53e20234541862691602372bc7f904547dc62
Aleksey Klimenko <klimenkoaleksey.prog@gmail.com>
2022-06-14 16:17:01 +0300

    feat: add react-ga library

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,24 +1,33 @@
-import { FunctionComponent, MouseEvent } from "react";
+import { FunctionComponent, MouseEvent } from 'react';
+import useAnalyticsEventTracker from '../hooks/useAnalyticsEventTracker';
 
 export interface IButton {
-	text: string;
-	onClick?: () => void;
-	disabled?: boolean;
-	externalClasses?: string[];
+  text: string;
+  onClick?: () => void;
+  disabled?: boolean;
+  externalClasses?: string[];
 }
 
 const Button: FunctionComponent<IButton> = (props) => {
-	const { text, onClick, externalClasses, disabled } = props;
-	return (
-		<button
-			disabled={disabled}
-			onClick={onClick}
-			className={`bg-zinc-700 text-white py-3 px-4 rounded-lg font-semibold ${externalClasses}`}
-      id={`${text.toLowerCase().replace(' ', '-')}-button`}
-		>
-			{text}
-		</button>
-	);
+  const { text, onClick, externalClasses, disabled } = props;
+  const gaEventTracker = useAnalyticsEventTracker('click');
+
+  const handleClick = () => {
+    gaEventTracker(text.toLowerCase().replace(' ', '-'));
+    if (onClick) {
+      onClick();
+    }
+  };
+
+  return (
+    <button
+      disabled={disabled}
+      onClick={handleClick}
+      className={`bg-zinc-700 text-white py-3 px-4 rounded-lg font-semibold ${externalClasses}`}
+    >
+      {text}
+    </button>
+  );
 };
 
 export default Button;
