e8c9faf7953a739cc6cfcf5d2bf3d430e304907d
AndreyBS1 <andrey.babnekov.00@gmail.com>
2022-08-08 17:18:35 +0300

    feat: add orders list to home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,20 @@
+import { useEthers } from '@usedapp/core'
+import { useEffect } from 'react'
+
 import searchIcon from '../assets/icons/search.svg'
 import Button from '../components/Button'
-// import OrderCard from '../components/OrderCard'
-// import Spinner from '../components/Spinner'
+import OrderCard from '../components/OrderCard'
+import Spinner from '../components/Spinner'
+import { useOrders } from '../hooks/useOrders'
 
 function Home() {
+  const orders = useOrders()
+  const { activateBrowserWallet } = useEthers()
+
+  useEffect(() => {
+    activateBrowserWallet()
+  }, [])
+
   return (
     <div className="container mx-auto pt-12">
       <h1 className="text-center mt-20 mb-32 font-bold text-6xl w-3/4 mx-auto leading-snug">
@@ -15,33 +26,36 @@ function Home() {
         </span>{' '}
         to the existing
       </h1>
+
       <div className="flex justify-between">
-        <div className="bg-white rounded border-black border py-2 py-3 px-4 w-1/3 flex items-center">
+        <div className="bg-white rounded border-black border py-3 px-4 w-1/3 flex items-center">
           <img width="16" height="17" src={searchIcon} alt="" className="mr-3" />
           <input
             type="text"
             name=""
             id=""
             className="flex-1 text-dark-gray font-semibold self-stretch focus:outline-none placeholder:text-dark-gray"
-            placeholder="Search by smart-contract adress"
+            placeholder="Search by smart-contract address"
           />
         </div>
-        <Button text="Create order"></Button>
+        <Button text="Create order" />
       </div>
-      {/* {!filteredOrders && (
-        <div className="my-20">
-          <Spinner></Spinner>
+
+      {orders ? (
+        <div className="grid md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 xl gap-4 mt-10">
+          {orders?.map((order) => (
+            <OrderCard
+              key={`${order.id}${order.collectionAddress}`}
+              data={order}
+              // onClick={() => navigate('/swap', { state: order })}
+            />
+          ))}
         </div>
-      )} */}
-      <div className="grid md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 xl gap-4 mt-10">
-        {/* {filteredOrders?.map((order) => (
-          <OrderCard
-            key={order.id + order.collectionAddress}
-            data={order}
-            onClick={() => navigate('/swap', { state: order })}
-          ></OrderCard>
-        ))} */}
-      </div>
+      ) : (
+        <div className="my-20">
+          <Spinner />
+        </div>
+      )}
     </div>
   )
 }
