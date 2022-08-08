6386c905ddd7274a36d9abd4be9c4c7cfc67d248
AndreyBS1 <andrey.babnekov.00@gmail.com>
2022-08-08 13:31:13 +0300

    init

---
+++ src/pages/404/index.page.tsx
@@ -0,0 +1,9 @@
+import type * as React from 'react'
+
+interface IErrorPageProps {}
+
+const ErrorPage: React.FunctionComponent<IErrorPageProps> = (props) => {
+  return <>404 - page not found</>
+}
+
+export default ErrorPage
