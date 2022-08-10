2c46532ae648f7abd58c8d7cc7dbbd58e5a3ecc5
AndreyBS1 <andrey.babnekov.00@gmail.com>
2022-08-10 12:45:21 +0300

    docs: add ts docs for useQuickSwap hook

--- src/hooks/useQuickSwap.ts
+++ src/hooks/useQuickSwap.ts
@@ -3,11 +3,22 @@ import type {
   SignedNftOrderV4Serialized,
   SwappableAssetV4,
 } from '@traderxyz/nft-swap-sdk'
+import { ContractReceipt } from 'ethers'
 import { useContext } from 'react'
 
 import { SwapSdkContext } from '../providers/swapSdkProvider'
 import { useCreateOrder } from './useCreateOrder'
 
+/**
+ * Get the quick swap function for the passed configuration
+ * @param makerAsset an asset (ERC20, ERC721, or ERC1155) the user has
+ * @param takerAsset an asset (ERC20, ERC721, or ERC1155) the user wants
+ * @param makerAddress wallet address of user who creates the order
+ * @param chainId id of the chain in which the transaction will be performed
+ * @param metadata an optional record object that will be stored with the order in the orderbook
+ * @param fees optional array that contents config for fee and royalties
+ * @returns a function to create a new order, look up an existing matching order, and execute the transaction
+ */
 export function useQuickSwap(
   makerAsset: SwappableAssetV4,
   takerAsset: SwappableAssetV4,
@@ -26,7 +37,7 @@ export function useQuickSwap(
     fees
   )
 
-  const quickSwap = async () => {
+  const quickSwap = async (): Promise<ContractReceipt | undefined> => {
     if (!nftSwap) return
 
     const newOrder = await createOrder()
@@ -41,11 +52,14 @@ export function useQuickSwap(
           nftTokenId: makerAsset.tokenId,
           nftType: makerAsset.type,
         })
+
         const orderToBuy = ordersData.orders.find((order) => order.sellOrBuyNft === 'buy')
         if (!orderToBuy) return
+
         matchingOrder = orderToBuy.order
         const matchTx = await nftSwap.matchOrders(newOrder, matchingOrder)
-        await matchTx.wait()
+        const matchTxReceipt = await matchTx.wait()
+        return matchTxReceipt
       }
 
       if (makerAsset.type === 'ERC20' && takerAsset.type !== 'ERC20') {
@@ -54,16 +68,22 @@ export function useQuickSwap(
           nftTokenId: takerAsset.tokenId,
           nftType: takerAsset.type,
         })
+
         const orderToBuy = ordersData.orders.find(
           (order) => order.sellOrBuyNft === 'sell'
         )
         if (!orderToBuy) return
+
         matchingOrder = orderToBuy.order
         const matchTx = await nftSwap.matchOrders(matchingOrder, newOrder)
-        await matchTx.wait()
+        const matchTxReceipt = await matchTx.wait()
+        return matchTxReceipt
       }
+
+      return undefined
     } catch (error) {
       console.error(error)
+      return undefined
     }
   }
 
